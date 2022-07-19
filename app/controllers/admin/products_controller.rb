ed970ff6a000ac7be3dc9bf7a2e8d3832522a027
vagrant <wong_michael@rogers.com>
2022-07-19 03:22:17 +0000

    Fix Bug: Missing Admin Security

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,10 @@
 class Admin::ProductsController < ApplicationController
 
+  user_name = ENV['USER_NAME']
+  user_password = ENV['USER_PASSWORD']
+
+  http_basic_authenticate_with name: user_name, password: user_password
+
   def index
     @products = Product.order(id: :desc).all
   end
