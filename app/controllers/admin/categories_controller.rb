ede03374f624e0f8111d5bf64829e21bd25f7959
vagrant <wong_michael@rogers.com>
2022-07-21 04:43:15 +0000

    Create controller for admin/categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,34 @@
+Admin::ProductsController < ApplicationController
+
+user_name = ENV['USER_NAME']
+user_password = ENV['USER_PASSWORD']
+
+http_basic_authenticate_with name: user_name, password: user_password
+
+def index
+  @categories = Category.all.order(:name)
+end
+
+def new
+  @categories = Category.new
+end
+
+def create
+  @categories = Category.new(category_params)
+
+  if @categories.save
+    redirect_to [:admin, :categories], notice: 'Category created!'
+  else
+    render :new
+  end
+end
+
+private
+
+def category_params
+  params.require(:category).permit(
+    :name
+  )
+end
+
+end
