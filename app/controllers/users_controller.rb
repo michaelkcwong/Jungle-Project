83dfe9aca3acec3604a8dc579a9c75c508c54af4
vagrant <wong_michael@rogers.com>
2022-07-26 04:03:23 +0000

    Add logic to create function and add the private user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UserController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      sessions[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
   end
+end
+end
 
+private
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 end
